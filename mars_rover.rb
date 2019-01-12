6a8473da793bdb22f6efe0aa5c4507e1260714fe
jasjammy <joanne.colaco@gmail.com>
2019-01-12 14:31:43 -0500

    mars_rover: add error state detection and exit - error out when rover is out of bounds

--- mars_rover.rb
+++ mars_rover.rb
@@ -6,7 +6,7 @@
 
 require 'stringio'
 
-def move_rover(upper_right, commands)
+def move_rover(upper_right= [5,5], start, commands)
 
   dir_hash = Hash.new()
   dir_hash['E'] = {'L':'N', 'R':'S', 'M': lambda {|x,y,o| [x+1,y,o] } }
@@ -14,7 +14,7 @@ def move_rover(upper_right, commands)
   dir_hash['S'] = {'L':'E', 'R':'W', 'M': lambda {|x,y,o| [x,y-1,o] } }
   dir_hash['W'] = {'L':'S', 'R':'N', 'M': lambda {|x,y,o| [x-1,y,o] } }
 
-  curr_coord = upper_right
+  curr_coord = start
 
   commands.each do |step|
     puts "before step : #{curr_coord}"
@@ -28,13 +28,36 @@ def move_rover(upper_right, commands)
       curr_coord = dir_hash[curr_coord[2]][:M][curr_coord[0], curr_coord[1], curr_coord[2]]
     else
       puts "Error: Invalid command input #{step}"
+      exit
     end
 
     puts "after step :#{curr_coord}"
+    # See that the coordinates are valid
+    if (curr_coord[0] > upper_right[0] || curr_coord[0] < 0 ) || (curr_coord[1] > upper_right[1] || curr_coord[1] < 0 )
+      puts "Error: Rover is out of bounds"
+      exit
+    end
   end
 
   puts curr_coord
 
 end
 
-move_rover([1,2,'N'], ['L','M','L','M','L','M','L','M','M'])
+move_rover([5,5,'N'], ['L','M','L','M','L','M','L','M','M'])
+
+# # take the input from command line
+#
+# if ARGV.length < 1
+#   puts "Too few arguments. Please give a file name as input"
+#   exit
+# end
+#
+# fptr = File.open($ARGV[0])
+#
+# while !fptr.eof?
+#   first_line = fptr.readline()
+#   upper_right = first_line.rstrip.split(" ")
+#   second_line = fptr.readline()
+#   commands = second_line.rstrip.split()
+#   move_rover(upper_right, commands)
+# end
