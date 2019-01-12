0b27fc32029f55bc60c4778d40b25f0ce85c4d07
jasjammy <joanne.colaco@gmail.com>
2019-01-12 15:39:03 -0500

    mars_rover: refactor to make a class Rover - adjust function calls in readfile script

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,42 +1,57 @@
 #!/bin/ruby
 #
-#
-# This script will read a text file and output the final position of each rover
-
+# This ruby file contains a class Rover and a script to get inputs
 
 require 'stringio'
 
-def move_rover(upper_right, start, commands)
+# This class encapsulates the details of the position of the Rover.
+# It also stores information about the plane the Rover is travelling on.
+class Rover
 
-  dir_hash = Hash.new()
-  dir_hash['E'] = {'L':'N', 'R':'S', 'M': lambda {|x,y,o| [x+1,y,o] } }
-  dir_hash['N'] = {'L':'W', 'R':'E', 'M': lambda {|x,y,o| [x,y+1,o] } }
-  dir_hash['S'] = {'L':'E', 'R':'W', 'M': lambda {|x,y,o| [x,y-1,o] } }
-  dir_hash['W'] = {'L':'S', 'R':'N', 'M': lambda {|x,y,o| [x-1,y,o] } }
+  # This hash stores the value of the coordinates and orientation when the step is 'L' or 'R' or 'M'
+  @@dir_hash = Hash.new()
 
-  curr_coord = [start[0].to_i, start[1].to_i, start[2]]
-
-  commands.each do |step|
-    case step
-    when 'L'
-      curr_coord[2] = dir_hash[curr_coord[2]][:L]
-    when 'R'
-      curr_coord[2] = dir_hash[curr_coord[2]][:R]
-    when 'M'
-      curr_coord = dir_hash[curr_coord[2]][:M][curr_coord[0], curr_coord[1], curr_coord[2]]
-    else
-      puts "Error: Invalid command input #{step}"
-      return
-    end
-
-    if (curr_coord[0] > upper_right[0] || curr_coord[0] < 0 ) || (curr_coord[1] > upper_right[1] || curr_coord[1] < 0 )
-      puts "Error: Rover is out of bounds. Occured at step #{step} with coordinates #{curr_coord.join(" ")}"
-      return
-    end
+  def initialize(upper_right, start, commands)
+    populate_dir_hash
+    @upper_right = upper_right
+    @start = start
+    @commands = commands
   end
 
-  return curr_coord
+  def move_rover
+    curr_coord = [@start[0].to_i, @start[1].to_i, @start[2]]
 
+    @commands.each do |step|
+      case step
+      when 'L'
+        curr_coord[2] = @@dir_hash[curr_coord[2]][:L]
+      when 'R'
+        curr_coord[2] = @@dir_hash[curr_coord[2]][:R]
+      when 'M'
+        curr_coord = @@dir_hash[curr_coord[2]][:M][curr_coord[0], curr_coord[1], curr_coord[2]]
+      else
+        puts "Error: Invalid command input #{step}"
+        return
+      end
+      # check that the rover has not gone out of bounds
+      if (curr_coord[0] > @upper_right[0] || curr_coord[0] < 0 ) || (curr_coord[1] > @upper_right[1] || curr_coord[1] < 0 )
+        puts "Error: Rover is out of bounds. Occured at step #{step} with coordinates #{curr_coord.join(" ")}"
+        return
+      end
+    end
+
+    return curr_coord
+
+  end
+
+  private
+
+  def populate_dir_hash
+    @@dir_hash['E'] = {'L':'N', 'R':'S', 'M': lambda {|x,y,o| [x+1,y,o] } }
+    @@dir_hash['N'] = {'L':'W', 'R':'E', 'M': lambda {|x,y,o| [x,y+1,o] } }
+    @@dir_hash['S'] = {'L':'E', 'R':'W', 'M': lambda {|x,y,o| [x,y-1,o] } }
+    @@dir_hash['W'] = {'L':'S', 'R':'N', 'M': lambda {|x,y,o| [x-1,y,o] } }
+  end
 end
 
 
@@ -61,6 +76,7 @@ while !fptr.eof?
   start = first_line.rstrip.split(" ")
   second_line = fptr.readline()
   commands = second_line.rstrip.split("")
-  result = move_rover(upper_right, start, commands)
+  rover = Rover.new(upper_right, start, commands)
+  result = rover.move_rover
   puts result.join(" ") unless result.nil?
 end
