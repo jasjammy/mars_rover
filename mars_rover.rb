decf4aedd89188111ede02fda56547acb6fedafd
jasjammy <joanne.colaco@gmail.com>
2019-01-12 15:05:33 -0500

    mars_rover: read from file and add input.txt - add code to read from file - check that the file is not empty

--- mars_rover.rb
+++ mars_rover.rb
@@ -6,7 +6,7 @@
 
 require 'stringio'
 
-def move_rover(upper_right= [5,5], start, commands)
+def move_rover(upper_right, start, commands)
 
   dir_hash = Hash.new()
   dir_hash['E'] = {'L':'N', 'R':'S', 'M': lambda {|x,y,o| [x+1,y,o] } }
@@ -14,11 +14,11 @@ def move_rover(upper_right= [5,5], start, commands)
   dir_hash['S'] = {'L':'E', 'R':'W', 'M': lambda {|x,y,o| [x,y-1,o] } }
   dir_hash['W'] = {'L':'S', 'R':'N', 'M': lambda {|x,y,o| [x-1,y,o] } }
 
-  curr_coord = start
+  curr_coord = [start[0].to_i, start[1].to_i, start[2]]
 
   commands.each do |step|
-    puts "before step : #{curr_coord}"
-    puts "step : #{step}"
+    # puts "before step : #{curr_coord}"
+    # puts "step : #{step}"
     case step
     when 'L'
       curr_coord[2] = dir_hash[curr_coord[2]][:L]
@@ -31,33 +31,40 @@ def move_rover(upper_right= [5,5], start, commands)
       exit
     end
 
-    puts "after step :#{curr_coord}"
+    # puts "after step :#{curr_coord}"
     # See that the coordinates are valid
     if (curr_coord[0] > upper_right[0] || curr_coord[0] < 0 ) || (curr_coord[1] > upper_right[1] || curr_coord[1] < 0 )
-      puts "Error: Rover is out of bounds"
+      puts "Error: Rover is out of bounds. Occured at step #{step} with coordinates #{curr_coord.join(" ")}"
       exit
     end
   end
 
-  puts curr_coord
+  return curr_coord
 
 end
 
-move_rover([5,5,'N'], ['L','M','L','M','L','M','L','M','M'])
 
-# # take the input from command line
-#
-# if ARGV.length < 1
-#   puts "Too few arguments. Please give a file name as input"
-#   exit
-# end
-#
-# fptr = File.open($ARGV[0])
-#
-# while !fptr.eof?
-#   first_line = fptr.readline()
-#   upper_right = first_line.rstrip.split(" ")
-#   second_line = fptr.readline()
-#   commands = second_line.rstrip.split()
-#   move_rover(upper_right, commands)
-# end
+# get input from command line
+if ARGV.length < 1
+  puts "Too few arguments. Please give a file name as input"
+  exit
+end
+
+filename = ARGV[0].to_s
+if File.zero?(filename) then
+  puts "Error: #{filename} is empty. Please provide a file with contents."
+  exit
+end
+
+fptr = File.open(filename,'r')
+# read upper right coordinates from the first line of the file.
+upper_right = fptr.readline().rstrip.split(" ").map(&:to_i)
+
+while !fptr.eof?
+  first_line = fptr.readline()
+  start = first_line.rstrip.split(" ")
+  second_line = fptr.readline()
+  commands = second_line.rstrip.split("")
+  result = move_rover(upper_right, start, commands)
+  puts result.join(" ") unless result.nil?
+end
